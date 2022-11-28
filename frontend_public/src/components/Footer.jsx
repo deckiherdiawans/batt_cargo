c431b0a2e956a8c22b7439f8a96070ec80289795
Decki ‪ <d.herdiawan.s@gmail.com>
2022-11-28 22:41:08 +0700

    Frontend public progress.

--- frontend_public/src/components/Footer.jsx
+++ frontend_public/src/components/Footer.jsx
@@ -14,29 +14,29 @@ const Footer = () => {
         <Col md={4} className="mb-4">
           <Col xs={12} className="fw-bold mb-2 text-center">Kontak Kami</Col>
           <Row xs={12}>
-            <Col xs={1}><Icon.PinMapFill /></Col>
-            <Col xs={11} className="mb-1">
+            <Col xs={2}><Icon.PinMap /></Col>
+            <Col xs={10} className="mb-1">
               <span className="fw-bold text-muted">Kantor Pusat</span><br />
               Jl. Samoja, RT. 06 RW.09, Kecamatan Batununggal, Bandung
             </Col>
           </Row>
           <Row xs={12}>
-            <Col xs={1}><Icon.Clock /></Col>
-            <Col xs={11} className="mb-1">
+            <Col xs={2}><Icon.Clock /></Col>
+            <Col xs={10} className="mb-1">
               <span className="fw-bold text-muted">Jam Kerja</span><br />
               Senin s/d. Sabtu, jam 10:00 s/d. 20:00 WIB
             </Col>
           </Row>
           <Row xs={12}>
-            <Col xs={1}><Icon.Envelope /></Col>
-            <Col xs={11} className="mb-1">
+            <Col xs={2}><Icon.Envelope /></Col>
+            <Col xs={10} className="mb-1">
               <span className="fw-bold text-muted">Email</span><br />
               irwan@battcargo.com
             </Col>
           </Row>
           <Row xs={12}>
-            <Col xs={1}><Icon.Whatsapp /></Col>
-            <Col xs={11} className="mb-1">
+            <Col xs={2}><Icon.Whatsapp /></Col>
+            <Col xs={10} className="mb-1">
               <span className="fw-bold text-muted">WhatsApp</span><br />
               <a href="https://api.whatsapp.com/send?phone=6288218568571" target="_blank" className="link-custom">
                 +6288218568571
--- frontend_public/src/views/Home.jsx
+++ frontend_public/src/views/Home.jsx
@@ -18,25 +18,46 @@ const Home = () => {
           </Carousel>
         </Container>
       </header>
-      <Container>
-        <Row>
-          <Col md={4} className="d-flex">
-            <Card className="mb-4 w-100">
-              <Card.Body className="d-flex flex-column">
-                <Form.Label htmlFor="nomor_resi" className="fw-bold">Lacak Pengiriman</Form.Label>
-                <Row className="d-flex flex-row justify-content-between">
-                  <Col sm={8} className="p-0">
-                    <Form.Control type="text" id="nomor_resi" name="nomor_resi" placeholder="Masukkan nomor resi" />
-                  </Col>
-                  <Col sm={4} className="p-0 pl-auto">
-                    <Button className="bg-batt button-custom">Lacak</Button>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <Row className="form-custom">
+        <Col md={4} className="d-flex">
+          <Card className="mb-4 w-100">
+            <Card.Body className="d-flex flex-column">
+              <Form.Label htmlFor="nomor_resi" className="fw-bold">Lacak Pengiriman</Form.Label>
+              <div className="d-flex flex-row justify-content-between">
+                <Form.Control type="text" id="nomor_resi" name="nomor_resi" className="w-75" placeholder="Masukkan nomor resi" />
+                <Button className="bg-batt button-custom">Lacak</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="d-flex">
+          <Card className="mb-4 w-100">
+            <Card.Body className="d-flex flex-column">
+              <Form.Label className="fw-bold">Cek Tarif</Form.Label>
+              <div className="d-flex flex-row justify-content-between cek-tarif-custom">
+                <Form.Control type="text" id="asal" name="asal" className="w-25" placeholder="Asal" />
+                <Form.Control type="text" id="tujuan" name="tujuan" className="w-25" placeholder="Tujuan" />
+                <Form.Control type="text" id="berat" name="berat" className="w-25" placeholder="Berat (Kg)" />
+                <span className="mt-1 mb-2 small cek-tarif-tag-1">Masukkan asal, tujuan, & berat barang Anda</span>
+                <Button className="bg-batt button-custom">Cek Tarif</Button>
+              </div>
+              <span className="mt-2 small cek-tarif-tag-2">Masukkan asal, tujuan, & berat barang Anda</span>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="d-flex">
+          <Card className="mb-4 w-100">
+            <Card.Body className="d-flex flex-column">
+              <Form.Label htmlFor="kk" className="fw-bold">Lokasi Agen</Form.Label>
+              <div className="d-flex flex-row justify-content-between">
+                <Form.Control type="text" id="kk" name="kk" className="w-75" placeholder="Kecamatan/Kota" />
+                <Button className="bg-batt button-custom">Cari</Button>
+              </div>
+              <span className="mt-2 small">Cari lokasi terdekat</span>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
