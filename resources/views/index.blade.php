10f51c6eb03501c8b06027ee6949e6240455b484
Decki ‪ <d.herdiawan.s@gmail.com>
2022-12-08 23:31:31 +0700

    Test run on development.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,11 @@
-@extends('main')
+@extends('layouts/main')
 
-@section('title', 'Home')
+@extends('partials/head')
+@extends('partials/nav')
+@extends('partials/scripts')
+
+@section('title', 'Batt Cargo | Logistic & Distribution')
 
 @section('content')
-
+<h1>What's up motherfuckers!!!</h1>
 @endsection
