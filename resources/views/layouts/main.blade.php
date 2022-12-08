f3dcc91ee6d390e1799e331240f3b7e55c3c4521
Decki ‪ <d.herdiawan.s@gmail.com>
2022-12-08 23:29:37 +0700

    Arrange views directory structure.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,8 +2,13 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  
+  @yield('head')
+  
   <title>@yield('title')</title>
 </head>
 <body>
