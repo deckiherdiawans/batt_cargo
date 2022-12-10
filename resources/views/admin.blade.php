b74ef0a4ee838dea186f863f4cc16ada4336a7cc
Decki ‪ <d.herdiawan.s@gmail.com>
2022-12-10 23:54:39 +0700

    Add index file for admin page.

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+
+@extends('partials/head')
+@extends('partials/admin-nav')
+@extends('partials/scripts')
+
+@section('title', 'Admin - ')
+
+@section('content')
+
+@endsection
