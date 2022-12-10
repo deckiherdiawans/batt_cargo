30f6d91bdffd4e0beb3977211387fd2396279a1f
Decki ‪ <d.herdiawan.s@gmail.com>
2022-12-10 23:59:09 +0700

    Add css files.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -17,5 +17,7 @@
   <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png">
   <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png">
 
-  @vite(["resources/js/app.js"])
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/sidebars.css" rel="stylesheet">
+  <link href="css/app.css" rel="stylesheet">
 @endsection
